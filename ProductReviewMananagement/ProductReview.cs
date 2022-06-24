dd714c07f12b0eabce39b6850be1b9c612a089d8
biplabiswas1234 <biplabiswas1234@gmail.com>
2022-06-24 18:24:45 +0530

    [Biplab] Add: Creating product review table

---
+++ ProductReviewMananagement/ProductReview.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductReviewMananagementLinq
+{
+    public class ProductReview
+    {
+        public int ProductID { get; set; }
+        public int UserID { get; set; }
+        public double Rating { get; set; }
+        public string Review { get; set; }
+        public bool IsLike { get; set; }
+    }
+}
---
+++ ProductReviewMananagement/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ProductReviewMananagementLinq
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome in Product Review Management");
+            ProductReviewManagement productReview = new ProductReviewManagement();
+            productReview.AddProductReviewManagement();
+        }
+    }
+}
